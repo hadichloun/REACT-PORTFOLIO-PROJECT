a7975c49f9cbac19618a2daf3e51b666885464a8
hadichloun <hadi_960@hotmail.com>
2021-01-10 23:18:00 -0600

    populating portfolio manager state with API call Data

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,14 +1,6 @@
+  
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
-
-      <div>
-        <Link to="/about-me">Read more about myself</Link>
-      </div>
-    </div>
-  );
+  return <div>About</div>;
 }
---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Axios from "axios";
+
+export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    }
+  }
+
+  getPortfolioItems() {
+    Axios.get("https://hadichloun.devcamp.spcae/portfolio/portfolio_items", { 
+      withCredentials: true
+    }).then(response => {
+      this.setState({
+        portfolioItems: [...response.data.portfolio_items]
+      });
+    })
+    .catch(error => {
+      console.log("error in getPortfolioItems", error);
+    })
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>Portfolio form....</h1>
+        </div>
+
+        <div className="right-column">
+          <h1>Portfolio sidebar....</h1>
+        </div>
+      </div>
+    );
+  }
+}
