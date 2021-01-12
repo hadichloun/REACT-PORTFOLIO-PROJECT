f4056d0bc3567440f4bc400997f82fad6018c867
hadichloun <hadi_960@hotmail.com>
2021-01-11 21:39:57 -0600

    create portfolio form and call it from parent

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import Axios from "axios";
+import axios from "axios";
 
-import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list"
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
   constructor() {
@@ -9,30 +10,51 @@ export default class PortfolioManager extends Component {
 
     this.state = {
       portfolioItems: []
-    }
+    };
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
+      this
+    );
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+  }
+
+  handleSuccessfulFormSubmission(portfolioItem) {
+    // TODO
+    // update the portfolioItems state
+    // and add the portfolioItem to the list
+  }
+
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError error", error);
   }
 
   getPortfolioItems() {
-    Axios.get("https://jordan.devcamp.space/portfolio/portfolio_items", { 
-      withCredentials: true
-    }).then(response => {
-      this.setState({
-        portfolioItems: [...response.data.portfolio_items]
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
       });
-    })
-    .catch(error => {
-      console.log("error in getPortfolioItems", error);
-    })
   }
 
   componentDidMount() {
     this.getPortfolioItems();
   }
+
   render() {
     return (
       <div className="portfolio-manager-wrapper">
         <div className="left-column">
-          <h1>Portfolio form....</h1>
+          <PortfolioForm
+            handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+            handleFormSubmissionError={this.handleFormSubmissionError}
+          />
         </div>
 
         <div className="right-column">
